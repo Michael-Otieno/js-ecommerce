e425d6a74ab55bd5d94d4411a1f50f8f2851d675
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-01-23 16:39:28 +0300

    correct navbar

--- public/js/token.js
+++ public/js/token.js
@@ -30,3 +30,45 @@ const compareToken = (token, key) => {
 
 }
 
+
+
+// common functions needed for sellers dashboard
+
+// send data function
+const sendData = (path, data) =>{
+    fetch(path, {
+        method:'post',
+        headers: new Headers({'Content-Type':'application/json'}),
+        body: JSON.stringify(data)
+    }).then((res) => res.json())
+    .then(response =>{
+        // console.log(response)
+        processData(response);
+    });
+}
+
+const processData = (data) => {
+    loader.style.display = null;
+    if(data.alert){
+        alert(data.alert)
+    }else if(data.name){
+        // create auth token
+        // console.log(data)
+        data.authToken = generateToken(data.email);
+        sessionStorage.user = JSON.stringify(data);
+        location.replace('/');
+    }
+}
+
+
+
+
+// const showAlert = (msg) => {
+//     let alertBox = document.querySelector('.alert-box');
+//     let alertMsg = document.querySelector('.alert-msg');
+//     alertMsg.innerHTML = msg;
+//     alertBox.classList.add('show');
+//     setTimeout(() => {
+//         alertBox.classList.remove('show');
+//     }, 3000);
+// }
